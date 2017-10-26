7f87d49f497a09b32fb4c005d9dd4ffb8cca4344
Benoit HIVERT <hivert.benoit@gmail.com>
2017-10-26 19:30:20 +0200

    state.[ch] added

---
+++ state.c
@@ -0,0 +1,32 @@
+/* 
+ * File:   state.c
+ * Author: bhivert
+ *
+ * Created on June 25, 2017, 7:48 PM
+ */
+
+#include "state.h"
+
+void					state_init(state_t *state, state_fct_t *fcts[], id_t nids) {
+	state->id = (id_t)~0;
+	state->fcts = fcts;
+	state->nids = nids;
+}
+
+void					state_reset(state_t *state) {
+	state->id = 0;
+}
+
+void					state_stop(state_t *state) {
+	state->id = ~0;
+}
+
+bool					state_isstopped(state_t state) {
+	return state.id == (id_t)~0 || !(state.id < state.nids);
+}
+
+void					state_exec(state_t *state) {
+	if (!state_isstopped(*state)) {
+		state->id = state->fcts[state->id](*state);
+	}
+}
---
+++ state.h
@@ -0,0 +1,29 @@
+/* 
+ * File:   state.h
+ * Author: bhivert
+ *
+ * Created on June 25, 2017, 7:48 PM
+ */
+
+#ifndef STATE_H
+# define STATE_H
+
+# include <stdbool.h>
+
+typedef unsigned short	id_t;
+typedef struct state_s	state_t;
+typedef id_t			(state_fct_t)(state_t);
+
+struct					state_s {
+	id_t				id;
+	id_t				nids;
+	state_fct_t			**fcts;
+};
+
+void					state_init(state_t *state, state_fct_t *fcts[], id_t nids);
+void					state_reset(state_t *state);
+void					state_stop(state_t *state);
+bool					state_isstopped(state_t state);
+void					state_exec(state_t *state);
+
+#endif	/* STATES_H */
